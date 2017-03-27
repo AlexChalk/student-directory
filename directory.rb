a4fdadb1760894fd690f7bb23d74a254b7fa1f9a
Alex Chalk <alexchalk17@gmail.com>
2017-03-27 11:04:04 -0400

    Added 8:1 and 8:2

--- directory.rb
+++ directory.rb
@@ -1,3 +1,18 @@
+# Let's put all students into an array
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -23,8 +38,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, number|
+    if student[:name].downcase[0] == "t"
+      puts "#{number+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
@@ -32,7 +49,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 #nothing happens until we call the methods
-students = input_students
+#students = input_students
 print_header
 print(students)
 print_footer(students)
